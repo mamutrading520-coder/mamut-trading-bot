97aecfbe3b55257222517e7d82b4d1a9af00fd7c
mamutcode <mamutrading520@gmail.com>
2026-03-19 12:27:28 -0500

    Delete tests directory

--- tests/test_smoke.py
+++
@@ -1,9 +0,0 @@
-﻿"""Smoke tests for main functionality"""
-import pytest
-
-def test_imports():
-    """Test that main modules can be imported"""
-    from config.settings import Settings
-    from core.orchestrator import Orchestrator
-    assert Settings is not None
-    assert Orchestrator is not None
